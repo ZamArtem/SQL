52cad08b8729f2c25b5ef00f9cc6717e190a9398
Zamishailo Artem <72965392+ZamArtem@users.noreply.github.com>
2022-09-09 16:43:56 +0200

    Create sulipy.sql

---
+++ kezdetek/sulipy.sql
@@ -0,0 +1,28 @@
+/*  1. Feladat
+Írj SQL utasítást amely az online tesztadatbázis 'Categories' táblájából lekérdezi
+a, az összes adatot,
+b, a 'CategoryName' oszlopba tartozó adatokat,
+c, a 'CategoryName' és 'Description' oszlopba tartozó adatokat!
+
+2. Feladat
+Írj SQL utasítást amely az online tesztadatbázis 'Products' táblájából lekérdezi
+a, a 'ProductName', 'Unit' és 'Price' oszlopba tartozó adatokat,
+b, azon termékek nevét és árát, amelyek ára 20-nál kevesebb,
+c, azon termékek nevét és árát, amelyek ára 20 és 30 közé esik,
+d, azon termékek nevét és árát, amelyek ára nem 10,
+e, a 'T' betűvel kezdődő temékek nevét,
+f, azon termékek nevét, amelynek neve a 'Louisiana' szóval kezdődik!
+
+3. Feladat
+Írj SQL utasítást amely az online tesztadatbázis 'Suppliers' táblájából lekérdezi az országokat ismétlődés nélkül!
+*/
+
+
+  -1/a, 
+  SELECT * FROM Categories;
+  -1/b,
+  SELECT CategoryName FROM Categories;
+  -1/c,
+  SELECT CategoryName, Description FROM Categories;
+  
+  -2/a
