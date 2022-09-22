2c5c8ac0baa040746aa4429e8b001e6b74b72b26
Zamishailo Artem <72965392+ZamArtem@users.noreply.github.com>
2022-09-22 23:57:07 +0200

    Update fugvenyek.sql

--- bovebben/fugvenyek.sql
+++ bovebben/fugvenyek.sql
@@ -1,1 +1,21 @@
+USE northwind;
+SELECT MIN(list_price) AS olcso FROM products;
+SELECT * FROM products ORDER BY list_price;
 
+USE northwind;
+SELECT MAX(list_price) AS draga FROM products;
+SELECT * FROM products ORDER BY list_price DESC;
+
+USE northwind;
+SELECT COUNT(description) FROM products;
+SELECT * FROM products WHERE description is NOT NULL;
+
+USE northwind;
+SELECT COUNT(id) FROM products WHERE list_price = 1.2;
+SELECT * FROM products ORDER BY list_price;
+
+USE northwind;
+SELECT SUM(list_price) FROM products WHERE id < 4;
+
+USE northwind;
+SELECT AVG(list_price) FROM products WHERE id < 4;
