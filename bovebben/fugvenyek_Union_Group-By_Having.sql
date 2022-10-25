d94b165e6061ef96585b321956d2cc8d17205b8b
Zamishailo Artem <72965392+ZamArtem@users.noreply.github.com>
2022-10-25 14:00:12 +0200

    Update fugvenyek_Union_Group-By_Having.sql

--- bovebben/fugvenyek_Union_Group-By_Having.sql
+++ bovebben/fugvenyek_Union_Group-By_Having.sql
@@ -1,1 +1,18 @@
+--1.1
+SELECT ship_state_province FROM orders
+UNION
+SELECT state_province FROM customers;
+--1.2
+SELECT ship_state_province FROM orders
+UNION ALL
+SELECT state_province FROM customers;
+--2
+SELECT COUNT(category) FROM products;
+--3
+SELECT COUNT(ship_city) FROM orders;
+--4
+SELECT COUNT(id), city
+FROM customers
+HAVING COUNT(id) > 3
+
 
